9b6bfce582486e3825c661b26affb848b2c61e5c
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-09 19:38:52 +0100

    initial commit

---
+++ src/main/java/com/ia/legislative/LegislativeApiApplication.java
@@ -0,0 +1,13 @@
+package com.ia.legislative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegislativeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegislativeApiApplication.class, args);
+	}
+
+}
