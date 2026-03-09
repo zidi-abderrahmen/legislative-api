ab82b4b1ce61b1c19e6cf1968e297bd08ae922d6
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-09 22:46:39 +0100

    feat: added the KeywordRepository interface with custom queries

---
+++ src/main/java/com/ia/legislative/repositories/KeywordRepository.java
@@ -0,0 +1,24 @@
+package com.ia.legislative.repositories;
+
+import com.ia.legislative.entities.Keyword;
+import com.ia.legislative.entities.Law;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+    Optional<Keyword> findByText(String text);
+
+    @Query(
+            value = "SELECT k.* FROM keywords k " +
+                    "JOIN law_keywords lk ON k.id = lk.keyword_id " +
+                    "JOIN laws l ON lk.law_id = l.id " +
+                    "WHERE to_tsvector('simple', k.text) @@ to_tsquery('simple', :query)",
+            nativeQuery = true
+    )
+    List<Keyword> searchByLawsNative(@Param("query") String query);
+}
