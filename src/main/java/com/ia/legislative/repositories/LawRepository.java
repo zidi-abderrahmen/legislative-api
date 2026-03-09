ee706910677b84fe5f59c3c5b66094ba03432d7f
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-09 22:33:31 +0100

    feat: added the LawRepository interface with custom queries

---
+++ src/main/java/com/ia/legislative/repositories/LawRepository.java
@@ -0,0 +1,24 @@
+package com.ia.legislative.repositories;
+
+import com.ia.legislative.entities.Law;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LawRepository extends JpaRepository<Law, Long> {
+
+    Optional<Law> findByTitle(String title);
+
+    @Query(
+            value = "SELECT l.* FROM laws l " +
+                    "JOIN law_keywords lk ON l.id = lk.law_id " +
+                    "JOIN searched_keywords k ON lk.keyword_id = k.id " +
+                    "WHERE to_tsvector('simple', k.text) @@ to_tsquery('simple', :query)",
+            nativeQuery = true
+    )
+    List<Law> searchByKeywordsNative(@Param("query") String query);
+
+}
