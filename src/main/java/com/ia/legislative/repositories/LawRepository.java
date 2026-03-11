fcf809d48e784965b40820910d7cb684a8cb9c32
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-11 02:12:06 +0100

    feat: updated the custom query of the search by text

--- src/main/java/com/ia/legislative/repositories/LawRepository.java
+++ src/main/java/com/ia/legislative/repositories/LawRepository.java
@@ -15,9 +15,7 @@ public interface LawRepository extends JpaRepository<Law, Long> {
 
     @Query(
             value = "SELECT l.* FROM laws l " +
-                    "JOIN law_keywords lk ON l.id = lk.law_id " +
-                    "JOIN searched_keywords k ON lk.keyword_id = k.id " +
-                    "WHERE to_tsvector('simple', k.text) @@ to_tsquery('simple', :query)",
+                    "WHERE to_tsvector('simple', l.title) @@ to_tsquery('simple', :query)",
             nativeQuery = true
     )
     List<Law> searchByKeywordsNative(@Param("query") String query);
